7517af7889f1050dd3ee237829cb6869e36df8f9
Ryan <ryan@scalr.com>
2025-05-08 13:54:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Define the project/modules resources here
 
 resource "aws_s3_bucket" "runtime-poc-bucket" {
-  bucket = format("%s-runtime-spacelift-poc-%s-bucket", var.aws_region_shorthand, var.stage)
+  bucket = format("%s-runtime-scalr-poc-%s-bucket", var.aws_region_shorthand, var.stage)
 
   tags = {
     SubSystem = "demo"
