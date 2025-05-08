09983e43f804d9b3181ba8c6ed507e33dfedb07e
Ryan <ryan@scalr.com>
2025-05-08 16:15:22 -0400

    Update hook.sh

--- hook.sh
+++ hook.sh
@@ -1,30 +1,26 @@
 #!/bin/bash
 
-#echo "Debug: ENV_SPECIFIC_VALUES_FILE is set to: '$ENV_SPECIFIC_VALUES_FILE'"
+echo "Debug: ENV_SPECIFIC_VALUES_FILE is set to: '$ENV_SPECIFIC_VALUES_FILE'"
 
-#if [ -n "$ENV_SPECIFIC_VALUES_FILE" ]; then
-#  if [ -f "$ENV_SPECIFIC_VALUES_FILE" ]; then
-#    echo "Debug: File '$ENV_SPECIFIC_VALUES_FILE' exists."
-#    export TF_CLI_ARGS_plan=-var-file="$PWD/$ENV_SPECIFIC_VALUES_FILE}"
-#  else
-#    echo "Debug: File '$ENV_SPECIFIC_VALUES_FILE' does not exist!"
-#  fi
-#else
-#  echo "Debug: ENV_SPECIFIC_VALUES_FILE is not set or is empty."
-#fi
-#
-#echo "Debug: Final TF_CLI_ARGS_plan value: '$TF_CLI_ARGS_plan'"
+if [ -n "$ENV_SPECIFIC_VALUES_FILE" ]; then
+  if [ -f "$ENV_SPECIFIC_VALUES_FILE" ]; then
+    echo "Debug: File '$ENV_SPECIFIC_VALUES_FILE' exists."
+    export TF_CLI_ARGS_plan=-var-file="$PWD/$ENV_SPECIFIC_VALUES_FILE.tfvars"
+  else
+    echo "Debug: File '$ENV_SPECIFIC_VALUES_FILE' does not exist!"
+  fi
+else
+  echo "Debug: ENV_SPECIFIC_VALUES_FILE is not set or is empty."
+fi
 
+echo "Debug: Final TF_CLI_ARGS_plan value: '$TF_CLI_ARGS_plan'"
+
+
+##THIS WORKS:
 ##!/bin/bash
 
 #if [ -f "$PWD/$ENV_SPECIFIC_VALUES_FILE.tfvars" ]; then
 #    export TF_CLI_ARGS_plan=-var-file="$PWD/$ENV_SPECIFIC_VALUES_FILE.tfvars"
 #fi
 
-#!/bin/bash
-
-if [ -f "$PWD/$ENV_SPECIFIC_VALUES_FILE.tfvars" ]; then
-    export TF_CLI_ARGS_plan=-var-file="$PWD/$ENV_SPECIFIC_VALUES_FILE.tfvars"
-fi
-
-echo "Debug: Final TF_CLI_ARGS_plan value: '$TF_CLI_ARGS_plan'"
+#echo "Debug: Final TF_CLI_ARGS_plan value: '$TF_CLI_ARGS_plan'"
